da920427813f97f2754d979f6dacae9ce58397f7
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2022-08-20 16:00:59 +0200

    encapsulation in java - zadanie domowe

--- src/DiscountApp.java
+++ src/DiscountApp.java
@@ -3,19 +3,17 @@ class DiscountApp {
         Client client1 = new Client("Jan", "Kowalski", true);
         double price1 = 1100;
 
-        Client client2 = new Client("Karol", "Nowak", false);
+        Client client2 = new Client(null, "Nowak", false);
         double price2 = 1100;
 
         DiscountService discountService = new DiscountService();
         double priceDiscount1 = discountService.calculateDiscountPrice(client1, price1);
         double priceDiscount2 = discountService.calculateDiscountPrice(client2, price2);
 
-        System.out.println("Witaj " + client1.getFirstName() + " " + client1.getLastName());
-        System.out.println("Kwota przed rabatem: " + price1);
-        System.out.println("Do zapłaty (po rabacie): " + priceDiscount1);
+        PrintService printService = new PrintService();
+        printService.printSummary(client1, price1, priceDiscount1);
+        printService.printSummary(client2, price2, priceDiscount2);
 
-        System.out.println("Witaj " + client2.getFirstName() + " " + client2.getLastName());
-        System.out.println("Kwota przed rabatem: " + price2);
-        System.out.println("Do zapłaty (po rabacie): " + priceDiscount2);
+
     }
 }
